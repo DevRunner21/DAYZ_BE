8e54a5678b0e68239054ab1822af820ee15ec96f
devrunner21 <devrunner21@gmail.com>
2022-01-09 20:29:44 +0900

    [feat] [#9] 주소 API들을 Swagger에 명세하라

--- src/main/java/com/dayz/member/controller/AddressController.java
+++ src/main/java/com/dayz/member/controller/AddressController.java
@@ -1,8 +1,10 @@
 package com.dayz.member.controller;
 
+import com.dayz.atelier.dto.response.ReadAtelierDetailResponse;
 import com.dayz.common.dto.CommonApiResponse;
 import com.dayz.member.dto.response.ReadAddressesResponse;
 import com.dayz.member.service.AddressService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "AddressController V1", value = "주소 CRUD API를 제공하는 Controller입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/addresses")
@@ -18,6 +21,14 @@ public class AddressController {
 
     private final AddressService addressService;
 
+    @ApiOperation(
+        value = "주소 목록 조회",
+        notes = "주소 목록을 조회합니다.",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiResponses({
+        @ApiResponse(code = 200, response = ReadAddressesResponse.class, message = "성공")
+    })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CommonApiResponse<ReadAddressesResponse> readAddresses() {
