62f0b7320915b5ff76244c04ff1ef62a0646481d
devrunner21 <devrunner21@gmail.com>
2022-01-09 20:43:08 +0900

    [feat] [#9] Member API들을 Swagger에 명세하라

--- src/main/java/com/dayz/member/dto/request/EditMemberAddressRequest.java
+++ src/main/java/com/dayz/member/dto/request/EditMemberAddressRequest.java
@@ -1,5 +1,6 @@
 package com.dayz.member.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import lombok.AccessLevel;
@@ -12,10 +13,12 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EditMemberAddressRequest {
 
+    @ApiModelProperty(value = "시/군/구 ID", dataType = "number", example = "1")
     @NotNull(message = "cityId is not null")
     @Positive(message = "cityId must be positive")
     private Long cityId;
 
+    @ApiModelProperty(value = "동 ID", dataType = "number", example = "1")
     @NotNull(message = "regionId is not null")
     @Positive(message = "regionId must be positive")
     private Long regionId;
--- src/main/java/com/dayz/member/dto/response/EditMemberAddressResponse.java
+++ src/main/java/com/dayz/member/dto/response/EditMemberAddressResponse.java
@@ -1,5 +1,6 @@
 package com.dayz.member.dto.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,12 +11,16 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EditMemberAddressResponse {
 
+    @ApiModelProperty(value = "시/군/구 ID", dataType = "number", example = "1")
     private Long cityId;
 
+    @ApiModelProperty(value = "시/군/구 이름", dataType = "string", example = "강남구")
     private String cityName;
 
+    @ApiModelProperty(value = "동 ID", dataType = "number", example = "1")
     private Long regionId;
 
+    @ApiModelProperty(value = "동 이름", dataType = "string", example = "자곡동")
     private String regionName;
 
     public static EditMemberAddressResponse of(Long cityId, String cityName, Long regionId, String regionName) {
