074d6d28877cb533e24a2574647e3f129f48290c
devrunner21 <devrunner21@gmail.com>
2022-02-04 19:15:14 +0900

    [refactor] [#23] Member 도메인의 소스를 점검하라
    - UseFlag 관련 조건 제거

--- src/main/java/com/dayz/member/domain/MemberRepository.java
+++ src/main/java/com/dayz/member/domain/MemberRepository.java
@@ -12,10 +12,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @EntityGraph(attributePaths = {"address", "permission"})
     Optional<Member> findById(@Param("id") Long id);
 
-    @Query("select m from Member m join fetch m.permission where m.username = :username and m.useFlag = true")
+    @Query("select m from Member m join fetch m.permission where m.username = :username")
     Optional<Member> findByUsername(@Param("username") String username);
 
-    @Query("select m from Member m join fetch m.permission  where m.provider = :provider and m.providerId = :providerId and m.useFlag = true")
+    @Query("select m from Member m join fetch m.permission  where m.provider = :provider and m.providerId = :providerId")
     Optional<Member> findByProviderAndProviderId(@Param("provider") String provider, @Param("providerId") String providerId);
 
 }
