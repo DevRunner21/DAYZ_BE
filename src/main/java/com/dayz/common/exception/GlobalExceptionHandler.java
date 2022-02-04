c104cd874586094143e573579bc0fcc565c2aad8
devrunner21 <devrunner21@gmail.com>
2022-02-04 15:23:39 +0900

    [refactor] [#23] ExceptionHandler에 경고로그를 제거하라

--- src/main/java/com/dayz/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dayz/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dayz.common.exception;
 
+import com.dayz.common.dto.ApiError;
 import com.dayz.common.dto.CommonApiResponse;
 import com.dayz.common.enums.ErrorInfo;
 import java.util.stream.Collectors;
@@ -19,24 +20,24 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonApiResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.error("{}", ex); /** todo: 에러 로그를 얼마나 찍어야하는지? */
+    public CommonApiResponse<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("{0}", ex); /* todo: 에러 로그를 얼마나 찍어야하는지? */
         return CommonApiResponse.error("METHOD_ARG_NOT_VALID", ex.getBindingResult().getAllErrors().stream()
                 .collect(Collectors.toMap(error -> ((FieldError) error).getField(), ObjectError::getDefaultMessage)));
     }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonApiResponse<?> handleBusinessExceptionException(BusinessException ex) {
-        log.error("{}", ex);
+    public CommonApiResponse<ApiError> handleBusinessExceptionException(BusinessException ex) {
+        log.error("{0}", ex);
 
         return CommonApiResponse.error(ex.getErrorInfo());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonApiResponse handleException(Exception ex) {
-        log.error("{}", ex);
+    public CommonApiResponse<ApiError> handleException(Exception ex) {
+        log.error("{0}", ex);
 
         return CommonApiResponse.error(ErrorInfo.UNKNOWN);
     }
