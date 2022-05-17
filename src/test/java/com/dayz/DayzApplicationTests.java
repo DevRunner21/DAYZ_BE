da31449206b151db6f24736610b18a7351ea10a6
devrunner21 <devrunner21@gmail.com>
2022-05-17 18:13:38 +0900

    [chore] 프로젝트 설정 구조를 변경하라
    - application.yml에서 공통되는 부분을 하나로 합침
    - FLyWay 의존성 임시 제거
    - gitignore 수정

--- src/test/java/com/dayz/DayzApplicationTests.java
+++ src/test/java/com/dayz/DayzApplicationTests.java
@@ -10,30 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class DayzApplicationTests {
-
-  @Autowired
-  private Jwt jwt;
-
-  @Autowired
-  private MemberRepository memberRepository;
 
   @Test
   void contextLoads() {
-
-    Member member = memberRepository.findById(1L).get();
-
-    String token = jwt.sign(
-        Jwt.Claims.from(
-            member.getId(),
-            member.getProviderId(),
-            member.getUsername(),
-            new String[]{member.getPermission().getName()}
-        )
-    );
-
-    System.out.println("Token = " + token);
 
   }
 
